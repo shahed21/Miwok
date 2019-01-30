3a9fd74c61c305841fb9a0ba6635ecfc42b99407
Shahed Rahim <shahed21@gmail.com>
2019-01-30 14:46:40 -0600

    Numbers Label updated

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.android.miwok;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * This method handles clicking on Numbers TextView
+     * @param view refering view
+     */
+    public void openNumbersList(View view) {
+        Intent intent = new Intent(this, NumbersActivity.class);
+        startActivity(intent);
+    }
+
+    /**
+     * This method handles clicking on Colors TextView
+     * @param view refering view
+     */
+    public void openColorsList(View view) {
+        Intent intent = new Intent(this, ColorsActivity.class);
+        startActivity(intent);
+    }
+
+    /**
+     * This method handles clicking on Phrases TextView
+     * @param view refering view
+     */
+    public void openPhrasesList(View view) {
+        Intent intent = new Intent(this, PhrasesActivity.class);
+        startActivity(intent);
+    }
+
+    /**
+     * This method handles clicking on Family Members TextView
+     * @param view refering view
+     */
+    public void openFamilyMembersList(View view) {
+        Intent intent = new Intent(this, FamilyMembersActivity.class);
+        startActivity(intent);
     }
 }
