ea87730aad625415573516f50e5d8a69fd84a180
Shahed Rahim <shahed21@gmail.com>
2019-01-30 15:53:27 -0600

    Numbers onClick EventListener added in Java code

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView numbers = (TextView)findViewById(R.id.numbers);
+        numbers.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                openNumbersList(view);
+            }
+        });
     }
 
     /**
