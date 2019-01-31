7d812e7eb7855cbc7f9c59e2df1eaba7874f8397
Shahed Rahim <shahed21@gmail.com>
2019-01-30 22:44:33 -0600

    Numbers being listed as TextViews

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,6 +2,11 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -9,5 +14,23 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+        ArrayList<String> wordList = new ArrayList<String>();
+        wordList.add(0,"one");
+        wordList.add(1,"two");
+        wordList.add(2,"three");
+        wordList.add(3,"four");
+        wordList.add(4,"five");
+        wordList.add(5,"six");
+        wordList.add(6,"seven");
+        wordList.add(7,"eight");
+        wordList.add(8,"nine");
+        wordList.add(9,"ten");
+        LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
+        for (int i =0; i<wordList.size(); i++) {
+            Log.v(this.getClass().getSimpleName(), wordList.get(i));
+            TextView wordView = new TextView(this);
+            wordView.setText(wordList.get(i));
+            rootView.addView(wordView);
+        }
     }
 }
