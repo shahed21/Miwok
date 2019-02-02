705172ee8157282bc4b2863def9736b029529818
Shahed Rahim <shahed21@gmail.com>
2019-02-02 02:17:04 -0600

    Done with Miwok App
    Signed-off-by: Shahed Rahim <shahed21@gmail.com>

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.support.design.widget.TabLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,86 +22,18 @@ public class MainActivity extends AppCompatActivity {
         // Find the view pager that will allow the user to swipe between fragments
         ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
 
+
         // Create an adapter that knows which fragment should be shown on each page
-        SimpleFragmentPagerAdapter adapter = new SimpleFragmentPagerAdapter(getSupportFragmentManager());
+        SimpleFragmentPagerAdapter adapter = new SimpleFragmentPagerAdapter(
+                getSupportFragmentManager(),
+                MainActivity.this);
 
         // Set the adapter onto the view pager
         viewPager.setAdapter(adapter);
-    }
 
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//        Log.v(LOG_TAG, "onCreate");
-//
-//        TextView numbers = (TextView)findViewById(R.id.numbers);
-//        numbers.setOnClickListener(new View.OnClickListener(){
-//            @Override
-//            public void onClick(View view) {
-//                openNumbersList(view);
-//            }
-//        });
-//        TextView colors = (TextView)findViewById(R.id.colors);
-//        colors.setOnClickListener(new View.OnClickListener(){
-//            @Override
-//            public void onClick(View view) {
-//                openColorsList(view);
-//            }
-//        });
-//        TextView familyMembers = (TextView)findViewById(R.id.family);
-//        familyMembers.setOnClickListener(new View.OnClickListener(){
-//            @Override
-//            public void onClick(View view) {
-//                openFamilyMembersList(view);
-//            }
-//        });
-//        TextView phrases = (TextView)findViewById(R.id.phrases);
-//        phrases.setOnClickListener(new View.OnClickListener(){
-//            @Override
-//            public void onClick(View view) {
-//                openPhrasesList(view);
-//            }
-//        });
-//    }
-//
-//    /**
-//     * This method handles clicking on Numbers TextView
-//     * @param view refering view
-//     */
-//    public void openNumbersList(View view) {
-//        Log.v(LOG_TAG, "openNumbersList");
-//        Intent intent = new Intent(this, NumbersFragment.class);
-//        startActivity(intent);
-//    }
-//
-//    /**
-//     * This method handles clicking on Colors TextView
-//     * @param view refering view
-//     */
-//    public void openColorsList(View view) {
-//        Log.v(LOG_TAG, "openColorsList");
-//        Intent intent = new Intent(this, ColorsFragment.class);
-//        startActivity(intent);
-//    }
-//
-//    /**
-//     * This method handles clicking on Phrases TextView
-//     * @param view refering view
-//     */
-//    public void openPhrasesList(View view) {
-//        Log.v(LOG_TAG, "openPhrasesList");
-//        Intent intent = new Intent(this, PhrasesFragment.class);
-//        startActivity(intent);
-//    }
-//
-//    /**
-//     * This method handles clicking on Family Members TextView
-//     * @param view refering view
-//     */
-//    public void openFamilyMembersList(View view) {
-//        Log.v(LOG_TAG, "openFamilyMembersList");
-//        Intent intent = new Intent(this, FamilyMembersFragment.class);
-//        startActivity(intent);
-//    }
+        // Setup the TabLayout
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        tabLayout.setupWithViewPager(viewPager);
+
+    }
 }
--- app/src/main/java/com/example/android/miwok/SimpleFragmentPagerAdapter.java
+++ app/src/main/java/com/example/android/miwok/SimpleFragmentPagerAdapter.java
@@ -1,13 +1,22 @@
 package com.example.android.miwok;
 
+import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
 public class SimpleFragmentPagerAdapter extends FragmentPagerAdapter {
+    private int[] tabTitles = {
+        R.string.category_numbers,
+        R.string.category_family,
+        R.string.category_colors,
+        R.string.category_phrases
+    };
+    private Context mContext;
 
-    public SimpleFragmentPagerAdapter(FragmentManager fm) {
+    public SimpleFragmentPagerAdapter(FragmentManager fm, Context context) {
         super(fm);
+        this.mContext = context;
     }
 
     @Override
@@ -26,5 +35,11 @@ public class SimpleFragmentPagerAdapter extends FragmentPagerAdapter {
     @Override
     public int getCount() {
         return 4;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        // Generate title based on item position
+        return mContext.getString(tabTitles[position]);
     }
 }
